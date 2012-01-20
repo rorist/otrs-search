82ea4bb544f1f9758fe9f3d4efa0d22840310b93
Aubort Jean-Baptiste <jean-baptiste.aubort@epfl.ch>
2012-01-20 13:07:22 +0100

    Reorganisation: use function for everything, check if in __main__, force option for create_session()

--- otrs_search.py
+++ otrs_search.py
@@ -9,6 +9,17 @@ OTRS_CONFIG = '~/.otrs-config'
 OTRS_PASSWD = '~/.otrs-passwd'
 OTRS_SESSION = '.otrs-session'
 
+req_amount = 1
+req_unit = 'day'
+req_ticketid = ''
+req_from = ''
+req_order = 'Up'
+#req_queue = ''
+#req_state = ''
+google = True
+verbose = False
+fulltext = True
+
 def usage():
     print 'Usage: %s <request>\nPour plus d\'informations: %s --help'%(sys.argv[0], sys.argv[0])
     sys.exit(0)
@@ -23,7 +34,7 @@ def help():
   -r, --reverse\t\tReverse the result order (always sorted by date)
   -v, --verbose\t\tDisplay what is being done
   -h\t\t\tYou are reading it
-   --id\t\t\tSearch ticket by id
+  --id\t\t\tSearch ticket by id
   --client\t\tSearch by customer email
   --queue\t\tTODO: Search by queue name
   --state\t\tTODO: Search by ticket state. Possible values: 'new', 'open', 'closed' '''
@@ -65,7 +76,9 @@ def get_headers():
         'Cookie': get_session(),
     }
 
-def create_session():
+def create_session(force=False):
+    if os.path.exists('%s/%s'%(tempfile.gettempdir(), OTRS_SESSION)) and not force:
+        return
     if verbose:
         print '\033[0;32mSession creation\033[0m'
     authfile = os.path.expanduser(OTRS_PASSWD)
@@ -119,166 +132,164 @@ def passphrase_cb(x,y,z):
 #            print '\033[0;31mYou are not logged in!\033[0m'
 #        create_session()
 
-##########
-## Main
+def get_args(args):
+    global verbose, req_amount, req_unit, req_ticketid, req_body, req_amount, req_unit, fulltext, req_from
+    try:
+        opts, reqs = getopt.getopt(args, 'rghva:u:', ['reverse', 'no-google', 'help', 'verbose', 'amount=', 'unit=', 'id', 'client='])
+        req_body = ' '.join(reqs)
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):
+                help()
+                sys.exit(0)
+            elif opt in ('-g', '--no-google'):
+                google = False
+            elif opt in ('-r', '--reverse'):
+                req_order = 'Down'
+            elif opt in ('-v', '--verbose'):
+                verbose = True
+            elif opt in ('-a', '--amount'):
+                req_amount = arg
+            elif opt in ('-u', '--unit'):
+                req_unit = arg
+            elif opt == '--id':
+                req_ticketid = req_body
+                req_body = ''
+                req_amount = ''
+                req_unit = ''
+                fulltext = False
+            elif opt == '--client':
+                req_from = arg
+                fulltext = False
+            #elif opt == '--queue':
+            #    req_queue = arg
+            #elif opt == '--state':
+            #    req_state = 'open'
+    except getopt.GetoptError:
+        usage()
+    
+    if verbose:
+        print 'Options in use:\n amount=%s, unit=%s'%(req_amount, req_unit)
 
-# Get arguments
-req_amount = 1
-req_unit = 'day'
-req_ticketid = ''
-req_from = ''
-req_order = 'Up'
-#req_queue = ''
-#req_state = ''
-google = True
-verbose = False
-fulltext = True
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'rghva:u:', ['reverse', 'no-google', 'help', 'verbose', 'amount=', 'unit=', 'id', 'client='])
-    req_body = ' '.join(args)
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            help()
-            sys.exit(0)
-        elif opt in ('-g', '--no-google'):
-            google = False
-        elif opt in ('-r', '--reverse'):
-            req_order = 'Down'
-        elif opt in ('-v', '--verbose'):
-            verbose = True
-        elif opt in ('-a', '--amount'):
-            req_amount = arg
-        elif opt in ('-u', '--unit'):
-            req_unit = arg
-        elif opt == '--id':
-            req_ticketid = req_body
-            req_body = ''
-            req_amount = ''
-            req_unit = ''
-            fulltext = False
-        elif opt == '--client':
-            req_from = arg
-            fulltext = False
-        #elif opt == '--queue':
-        #    req_queue = arg
-        #elif opt == '--state':
-        #    req_state = 'open'
-except getopt.GetoptError:
-    usage()
-
-if verbose:
-    print 'Options in use:\n amount=%s, unit=%s'%(req_amount, req_unit)
-
-if len(args) < 1 and fulltext:
-    usage()
+    if len(args) < 1 and fulltext:
+        usage()
 
 # Get configuration
-use_ssl = True
-uri_sch = 'https'
-c = ConfigParser.RawConfigParser()
-c.read(os.path.expanduser(OTRS_CONFIG))
-try:
-    HOST = c.get('Main', 'host')
-    GOOKEY = c.get('Main', 'google_key')
-    if '443' not in HOST:
-        use_ssl = False
-        uri_sch = 'http'
-except ConfigParser.NoSectionError, e:
-    sys.exit('You must create a config file in %s (%s)'%(OTRS_CONFIG, e))
-
-# Create session
-if not os.path.exists('%s/%s'%(tempfile.gettempdir(), OTRS_SESSION)):
-    create_session()
-
-# Search query
-params = urllib.urlencode({
-    'Body': req_body,
-    'TicketNumber': req_ticketid,
-    'From': req_from,
-    'Action': 'AgentTicketSearch',
-    'Subaction': 'Search',
-    'TimeSearchType': 'TimePoint',
-    'TicketCreateTimePointStart': 'Last',
-    'TicketCreateTimePoint': req_amount,
-    'TicketCreateTimePointFormat': req_unit,
-    'SortBy': 'Age',
-    'Order': req_order,
-    #'Queues': req_queue,
-    #'StateType': req_state,
-    'ResultForm': 'CSV',
-})
-
-# Get Tickets
-if verbose:
-    print '\033[0;32mSearching tickets ...\033[0m'
-if use_ssl:
-    conn = httplib.HTTPSConnection(HOST)
-else:
-    conn = httplib.HTTPConnection(HOST)
-try:
-    conn.request("POST", REQ, params, get_headers())
-    res = conn.getresponse()
-except Exception, e:
-    sys.exit(e)
-
-# Check response
-if res.getheader('Content-type') != 'text/csv; charset=utf-8':
-    create_session()
-    sys.exit('Session created, please make request again.')
-
-# Save result
-# TODO: Use filename given in http header
-csvdata = res.read()
-f = tempfile.mktemp()
-csvfile = open(f, 'w+')
-csvfile.write(csvdata)
-csvfile.seek(0)
-
-# Show tickets
-tickets_nb = 0
-tickets = {}
-if os.path.getsize(csvfile.name) > 1:
-    tickets_nb = len(open(f, 'rb').readlines()) - 1
-    tickets = csv.reader(csvfile, delimiter=';', quotechar='"')
-    # Language dependent field number
-    row = tickets.next()
-    id_queue = 0
-    id_title = 0
-    id_state = 0
-    # English
-    if row[2] == 'Created':
-        id_queue = row.index('Queue')
-        id_title = row.index('Subject')
-        id_state = row.index('State')
-    # French
-    elif row[2] == 'Créé':
-        id_queue = row.index('File')
-        id_title = row.index('Sujet')
-        id_state = row.index('État')
-
-print '\033[0;31mTicket(s) number: %i\033[0m'%tickets_nb
-
-for row in tickets:
+def get_conf():
+    global use_ssl, uri_sch, HOST, GOOKEY
+    use_ssl = True
+    uri_sch = 'https'
+    c = ConfigParser.RawConfigParser()
+    c.read(os.path.expanduser(OTRS_CONFIG))
     try:
-        ticketid = row[0]
-        date = row[2]
-        queue = unicode(row[id_queue], 'utf8')
-        title = unicode(row[id_title], 'utf8')
-        link = ''
-        state = ''
-        if row[id_state]=='open' or row[id_state]=='new':
-            state = '\033[1;31m[%s] \033[0m'%row[id_state].upper()
-    except IndexError, e:
-        print row
+        HOST = c.get('Main', 'host')
+        GOOKEY = c.get('Main', 'google_key')
+        if '443' not in HOST:
+            use_ssl = False
+            uri_sch = 'http'
+    except ConfigParser.NoSectionError, e:
+        sys.exit('You must create a config file in %s (%s)'%(OTRS_CONFIG, e))
+
+def get_tickets():
+    global req_body, req_ticketid, req_from, req_amount, req_unit, req_order, verbose, use_ssl, HOST, REQ
+    # Construct POST request
+    params = urllib.urlencode({
+        'Body': req_body,
+        'TicketNumber': req_ticketid,
+        'From': req_from,
+        'Action': 'AgentTicketSearch',
+        'Subaction': 'Search',
+        'TimeSearchType': 'TimePoint',
+        'TicketCreateTimePointStart': 'Last',
+        'TicketCreateTimePoint': req_amount,
+        'TicketCreateTimePointFormat': req_unit,
+        'SortBy': 'Age',
+        'Order': req_order,
+        #'Queues': req_queue,
+        #'StateType': req_state,
+        'ResultForm': 'CSV',
+    })
+
+    # Get Tickets
+    if verbose:
+        print '\033[0;32mSearching tickets ...\033[0m'
+    if use_ssl:
+        conn = httplib.HTTPSConnection(HOST)
+    else:
+        conn = httplib.HTTPConnection(HOST)
+    try:
+        conn.request("POST", REQ, params, get_headers())
+        res = conn.getresponse()
+    except Exception, e:
         sys.exit(e)
-    if google:
-        link = shorten('%s://%s%s?Action=AgentTicketZoom&TicketNumber=%s&ZoomExpand=1'%(uri_sch, HOST, REQ, int(ticketid)))
-    try:
-        print '\033[0;32m%s \033[0;34m%s \033[0;33m[%s] %s\033[0m\033[1m%s\033[0m\033[0m %s\033[0m'%(date, ticketid, queue, state, title, link)
-    except UnicodeDecodeError, e:
-        print e, row
+    
+    # Check response
+    if res.getheader('Content-type') != 'text/csv; charset=utf-8':
+        create_session(force=True)
+        sys.exit('Session created, please make request again.')
 
-csvfile.close()
+    return res
 
-print 'CSV: %s'%f
+def show_tickets(res):
+    # Save result
+    # TODO: Use filename given in http header
+    csvdata = res.read()
+    f = tempfile.mktemp()
+    csvfile = open(f, 'w+')
+    csvfile.write(csvdata)
+    csvfile.seek(0)
+
+    # Show tickets
+    tickets_nb = 0
+    tickets = {}
+    if os.path.getsize(csvfile.name) > 1:
+        tickets_nb = len(open(f, 'rb').readlines()) - 1
+        tickets = csv.reader(csvfile, delimiter=';', quotechar='"')
+        # Language dependent field number
+        row = tickets.next()
+        id_queue = 0
+        id_title = 0
+        id_state = 0
+        # English
+        if row[2] == 'Created':
+            id_queue = row.index('Queue')
+            id_title = row.index('Subject')
+            id_state = row.index('State')
+        # French
+        elif row[2] == 'Créé':
+            id_queue = row.index('File')
+            id_title = row.index('Sujet')
+            id_state = row.index('État')
+
+    print '\033[0;31mTicket(s) number: %i\033[0m'%tickets_nb
+
+    for row in tickets:
+        try:
+            ticketid = row[0]
+            date = row[2]
+            queue = unicode(row[id_queue], 'utf8')
+            title = unicode(row[id_title], 'utf8')
+            link = ''
+            state = ''
+            if row[id_state]=='open' or row[id_state]=='new':
+                state = '\033[1;31m[%s] \033[0m'%row[id_state].upper()
+        except IndexError, e:
+            print row
+            sys.exit(e)
+        if google:
+            link = shorten('%s://%s%s?Action=AgentTicketZoom&TicketNumber=%s&ZoomExpand=1'%(uri_sch, HOST, REQ, int(ticketid)))
+        try:
+            print '\033[0;32m%s \033[0;34m%s \033[0;33m[%s] %s\033[0m\033[1m%s\033[0m\033[0m %s\033[0m'%(date, ticketid, queue, state, title, link)
+        except UnicodeDecodeError, e:
+            print e, row
+
+    csvfile.close()
+
+    print 'CSV: %s'%f
+
+if __name__ == '__main__':
+    get_args(sys.argv[1:])
+    get_conf()
+    create_session()
+    res = get_tickets()
+    show_tickets(res)
 
